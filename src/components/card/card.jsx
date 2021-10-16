c83bcfc5a40daa809ef74e3b8403cdb99bc86bdd
TrungTuan <mynameistrungtuan@gmail.com>
2021-10-16 23:10:54 +0700

    Handle pagination and design product detail page

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,38 +1,41 @@
 import React from "react";
-import  "./card.scss";
+import "./card.scss";
 import { Button, Icon, Item } from "semantic-ui-react";
+import {useHistory} from 'react-router-dom';
+
+
 const Card = (props) => {
-    const item = props.product;
+  const item = props.product;
+  const history = useHistory();
+  const moveToDetail = () => {
+    history.push('/productDetail');
+  }
+
   return (
-    
-      <div className="card-container" onClick={() => {}}>
-        <img
-          className="image"
-          src={item.images[0]}
-        />
-        <h4 className="name">{item.name}</h4>
-        <p className="email d-flex">
-          Hãng: <span className="ml-1 text-success font-weight-bold">{item.brand}</span>
-        </p>
-        <p className="email d-flex name">
-          Chip xử lý:{" "}
-          <span className="ml-1 text-success font-weight-bold">
-            {item.cpu}
-          </span>
-        </p>
-        <p className="email d-flex">
-          Price: {item.price} <span className="ml-1 text-success font-weight-bold"></span>
-        </p>
-        
-        {/* <button onClick={() => { }} className="btn-success">
+    <div className="card-container" onClick={moveToDetail}>
+      <img className="image" src={item.images[0]} />
+      <h4 className="name">{item.name}</h4>
+      <p className="email d-flex">
+        Hãng:{" "}
+        <span className="ml-1 text-success font-weight-bold">{item.brand}</span>
+      </p>
+      <p className="email d-flex name">
+        Chip xử lý:{" "}
+        <span className="ml-1 text-success font-weight-bold">{item.cpu}</span>
+      </p>
+      <p className="email d-flex">
+        Price: {item.price}{" "}
+        <span className="ml-1 text-success font-weight-bold"></span>
+      </p>
+
+      {/* <button onClick={() => { }} className="btn-success">
               Mua ngay
             </button> */}
-        <Button onClick={() => {}} color="green">
-          <Icon name="eye" /> Xem sản phẩm
-        </Button>
-      </div>
-    
-  )
+      <Button onClick={() => {}} color="green">
+        <Icon name="eye" /> Xem sản phẩm
+      </Button>
+    </div>
+  );
 };
 
 export default Card;
