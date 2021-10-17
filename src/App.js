8f3ce651111a806402b06428ed367ef516d6ba05
TrungTuan <mynameistrungtuan@gmail.com>
2021-10-17 23:03:35 +0700

    Completed page product detail and show more the same product's brand in carousel

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from './pages/contact/contact';
 import Introduce from './pages/introduce/introduce';
 import Login from './pages/login/login';
 import NotFoundPage from './pages/notFound/notFound';
-import Register from './pages/register/regiter';
+import Register from './pages/register/regiter'
 import ProductDetail from './pages/productDetail/productDetail';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Route path='/introduce' exact component={Introduce} />
         <Route path='/login' exact component={Login} />
         <Route path='/register' exact component={Register} />
-        <Route path='/productDetail' exact component={ProductDetail} />
+        <Route path='/product/:id' exact component={ProductDetail} />
         <Route component={NotFoundPage} />     
       </Switch>
     </BrowserRouter>
--- src/pages/productDetail/productDetail.jsx
+++ src/pages/productDetail/productDetail.jsx
@@ -1,36 +1,115 @@
-import React from "react";
-import { Segment, Button, Table } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/navbar";
+import { Segment, Button, Table } from "semantic-ui-react";
 import "./productDetail.scss";
+import { useLocation } from "react-router-dom";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import axios from "axios";
+import CardItem from "../../components/cardItem/cardItem";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const ProductDetail = () => {
+  const [data, setData] = useState([]);
+  const [image, setimage] = useState("");
+  const [load, setLoad] = useState(true);
+  const location = useLocation();
+  const id = location.pathname?.split("product/")[1];
+  const [sameProduct, setSameProduct] = useState([]);
+  // const id = location.pathname?.replace('product/', '');
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    console.log("first");
+    fetchData();
+  }, [location]);
+
+  const fetchData = () => {
+    setLoad(true);
+    let url = `https://lap-center.herokuapp.com/api/product/getProductById/${id}`;
+    axios
+      .get(url)
+      .then(function (res) {
+        const data = res.data.response;
+        setData(data);
+        console.log("data detail: ", data);
+        setimage(data.images[0]);
+        setLoad(false);
+        fecthSameProduct(data.brand);
+      })
+      .catch(function (error) {
+        setLoad(false);
+        console.log("error: ", error);
+      });
+  };
+
+  const onChooseImage = (image) => {
+    setimage(image);
+  };
+
+  const fecthSameProduct = (brand) => {
+    // fetch API for get more product for this brand
+    setLoad(true);
+    axios
+      .get(
+        `https://lap-center.herokuapp.com/api/product?productBrand=${brand}&pageSize=10&pageNumber=1`
+      )
+      .then(function (response) {
+        console.log("product more: ", response.data.products);
+        setSameProduct(response.data.products);
+        setLoad(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setLoad(false);
+      });
+  };
   return (
     <div>
-        <Navbar/>
-      <Segment className="detail-segment-container">
-        <div className="detail-product-name">Ten Laptop</div>
+      <Navbar />
+      <Segment loading={load} className="detail-segment-container">
+        <div className="detail-product-name">{data.name}</div>
         <div className="detail-status">
           <p>Tình trạng: Còn hàng</p>
           <p style={{ marginLeft: "20px" }}>Bảo hành: 24 tháng</p>
         </div>
-          <hr style={{ width: "80%" }} />
+        <hr style={{ width: "80%" }} />
         <div className="detail-container">
-
           <div className="detail-left">
-            <img
-              className="detail-image"
-              src="https://philong.com.vn/media/product/24413-a477788167ae0ff601d0b182143d1ee8.jpg"
-              alt=""
-            />
+            <img className="detail-image" src={image} alt="" />
             <div className="detail-list-images">
-              {/* {data?.images?.map((item) => (
-                <img className="detail-image-small" src={item} alt="" onClick={() => onChooseImage(item)} />
-              ))} */}
+              {data?.images?.map((item) => (
+                <img
+                  className="detail-image-small"
+                  src={item}
+                  alt=""
+                  onClick={() => onChooseImage(item)}
+                />
+              ))}
             </div>
           </div>
           <div className="detail-main">
             <p>
-              Giá bán: <span>10000000 VND</span>
+              Giá bán: <span>{data.price}</span>
             </p>
             <div className="detail-discount">
               <div className="discount-top">
@@ -41,13 +120,12 @@ const ProductDetail = () => {
             <div className="detail-buy">
               <Button color="red">MUA NGAY</Button>
               <p>
-                GỌI NGAY <a href="tel:+84969442510"> 0379 26 6143 </a> ĐỂ GIỮ
+                GỌI NGAY <a href="tel:+84969442510"> 0522 564 268 </a> ĐỂ GIỮ
                 HÀNG
               </p>
             </div>
           </div>
           <div className="detail-right">
-              
             <div>
               <span>Điện thoại tư vấn - Đặt hàng</span>
               <ul>
@@ -66,44 +144,55 @@ const ProductDetail = () => {
             </div>
           </div>
         </div>
+
         <div className="specifications">
-        <Table celled fixed singleLine>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Phần cứng</Table.HeaderCell>
-        <Table.HeaderCell>Thông số kĩ thuật</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+          <Table celled fixed singleLine>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Phan cung</Table.HeaderCell>
+                <Table.HeaderCell>Thong so ki thuat</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>John</Table.Cell>
-        <Table.Cell>Approved</Table.Cell>
-        
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Jamie</Table.Cell>
-        <Table.Cell>Approved</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Jill</Table.Cell>
-        <Table.Cell>Denied</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Jill</Table.Cell>
-        <Table.Cell>Denied</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Jill</Table.Cell>
-        <Table.Cell>Denied</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Jill</Table.Cell>
-        <Table.Cell>Denied</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>Ram</Table.Cell>
+                <Table.Cell>{data.ram}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Man hinh</Table.Cell>
+                <Table.Cell>{data.monitor}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>CPU</Table.Cell>
+                <Table.Cell>{data.cpu}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Card man hinh</Table.Cell>
+                <Table.Cell>{data.card}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Model</Table.Cell>
+                <Table.Cell>{data.model}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>O cung</Table.Cell>
+                <Table.Cell>{data.disk}</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </div>
+        <div className="same-product">
+          <h3>Sản phẩm cùng thương hiệu</h3>
+          <hr />
+          <Carousel 
+            responsive={responsive}
+            showDots={true}
+          >
+            {sameProduct.map((item) => (
+              <CardItem product={item}/>
+            ))}
+          </Carousel>
         </div>
       </Segment>
     </div>
